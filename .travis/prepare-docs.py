f2ecadfea54f0c8e73f1018010694951a82069cc
Anton Rau <anton.rau@gmail.com>
2019-06-04 15:52:36 +0200

    Automatic generation of a search index.

--- .travis/prepare-docs.py
+++ .travis/prepare-docs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import fileinput
 
 SNIPPETS_ROOT = 'snippets'
 
 lines = ['- [Home](/README.md#getting-started)\n']
+paths = ['/']
+
 for root, dirs, files in os.walk(SNIPPETS_ROOT, topdown=True):
   for file in files:
-    if file.endswith("README.md"):
+    if file.endswith('README.md'):
       level = root.count(os.sep) - SNIPPETS_ROOT.count(os.sep) - 1
       path = os.path.join(root, file)
       with open(path, 'rt') as md:
@@ -16,7 +19,12 @@ for root, dirs, files in os.walk(SNIPPETS_ROOT, topdown=True):
       offset = '  ' * level
       line = f'{offset}- [{title}]({path})\n'
       lines.append(line)
-      print(title)
+      paths.append(path)
 
 with open('_sidebar.md', 'wt') as f:
   f.writelines(lines)
+
+
+for line in fileinput.FileInput('index.html', inplace=True):
+    line = line.replace('paths: []', f'paths: {paths}')
+    print(line, end='')
